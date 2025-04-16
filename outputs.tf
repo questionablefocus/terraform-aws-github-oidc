22eec8c92336364ca725bc5b386ab4c7acf197ff
Marcus Mo <marcus.mo@questionablefocus.com>
2025-04-16 15:48:35 +0100

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "read_write_role_arn" {
   description = "ARN of the Terraform read write IAM role"
-  value       = aws_iam_role.terraform_read_write.arn
+  value       = aws_iam_role.read_write.arn
 }
 
 output "read_write_role_name" {
   description = "Name of the Terraform read write IAM role"
-  value       = aws_iam_role.terraform_read_write.name
+  value       = aws_iam_role.read_write.name
 }
 
 output "read_only_role_arn" {
   description = "ARN of the Terraform read only IAM role"
-  value       = aws_iam_role.terraform_read_only.arn
+  value       = aws_iam_role.read_only.arn
 }
 
 output "read_only_role_name" {
   description = "Name of the Terraform read only IAM role"
-  value       = aws_iam_role.terraform_read_only.name
+  value       = aws_iam_role.read_only.name
 }
 
 output "oidc_provider_arn" {
