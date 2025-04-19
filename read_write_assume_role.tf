52c43222816388cabdd90a665af1506953e8de53
Marcus Mo <marcus.mo@questionablefocus.com>
2025-04-19 15:35:11 +0100

    Allow read/write assume role to not be created

--- read_write_assume_role.tf
+++ read_write_assume_role.tf
@@ -1,51 +1,51 @@
 data "aws_iam_policy_document" "read_write_assume_role" {
-  dynamic "statement" {
-    for_each = length(var.read_write_role_arns) > 0 ? [1] : []
+  statement {
+    sid     = "AllowAssumeRoleFromGitHubActions"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-    content {
-      sid     = "AllowAssumeRoleFromGitHubActions"
-      effect  = "Allow"
-      actions = ["sts:AssumeRole"]
-
-      principals {
-        type        = "AWS"
-        identifiers = var.read_write_role_arns
-      }
+    principals {
+      type        = "AWS"
+      identifiers = var.read_write_role_arns
     }
   }
 }
 
 resource "aws_iam_role" "read_write_assume_role" {
+  count = length(var.read_write_role_arns) > 0 ? 1 : 0
+
   name = "TerraformReadWriteAssume"
 
   assume_role_policy = data.aws_iam_policy_document.read_write_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "read_write_assume_access" {
-  role       = aws_iam_role.read_write_assume_role.name
+  count = length(var.read_write_role_arns) > 0 ? 1 : 0
+
+  role       = aws_iam_role.read_write_assume_role[0].name
   policy_arn = aws_iam_policy.read_write.arn
 }
 
 // Attach additional policies to the read write role
 resource "aws_iam_role_policy_attachment" "read_write_assume_policies" {
-  count = length(var.read_write_policy_arns)
+  count = length(var.read_write_role_arns) > 0 ? length(var.read_write_policy_arns) : 0
 
-  role       = aws_iam_role.read_write_assume_role.name
+  role       = aws_iam_role.read_write_assume_role[0].name
   policy_arn = var.read_write_policy_arns[count.index]
 }
 
 // Attach additional policy documents to the read write role
 resource "aws_iam_policy" "read_write_assume_additional" {
-  count = length(var.read_write_policy_documents)
+  count = length(var.read_write_role_arns) > 0 ? length(var.read_write_policy_documents) : 0
 
   name   = "TerraformReadWriteAssumeAdditional-${count.index}"
   policy = var.read_write_policy_documents[count.index]
 }
 
 resource "aws_iam_role_policy_attachment" "read_write_assume_additional_documents" {
-  count = length(var.read_write_policy_documents)
+  count = length(var.read_write_role_arns) > 0 ? length(var.read_write_policy_documents) : 0
 
-  role       = aws_iam_role.read_write_assume_role.name
+  role       = aws_iam_role.read_write_assume_role[0].name
   policy_arn = aws_iam_policy.read_write_assume_additional[count.index].arn
 }
 
@@ -59,12 +59,16 @@ data "aws_iam_policy_document" "allow_read_write_assume_role" {
 }
 
 resource "aws_iam_policy" "allow_read_write_assume_role" {
+  count = length(var.read_write_role_arns) > 0 ? 1 : 0
+
   name = "AllowReadWriteAssumeRole"
 
   policy = data.aws_iam_policy_document.allow_read_write_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "allow_read_write_assume_access" {
-  role       = aws_iam_role.read_write_assume_role.name
-  policy_arn = aws_iam_policy.allow_read_write_assume_role.arn
+  count = length(var.read_write_role_arns) > 0 ? 1 : 0
+
+  role       = aws_iam_role.read_write_assume_role[0].name
+  policy_arn = aws_iam_policy.allow_read_write_assume_role[0].arn
 }
